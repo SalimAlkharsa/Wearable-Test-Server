eae15d80a438198157fe57a2e01c717baacaf827
Salimalkharsa <salimalkharsa@gmail.com>
2023-11-03 14:54:02 -0500

    init

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Initialize an empty data store
+data_store = []
+
+# Define a route that only accepts POST requests
+@app.route('/post_endpoint', methods=['POST'])
+def post_request_handler():
+    # Check if the request contains JSON data
+    if request.is_json:
+        data = request.get_json()
+        data_store.append(data)  # Store the posted data
+        return jsonify({"message": "Received POST request", "data": data}), 200
+    else:
+        return jsonify({"error": "Request data must be in JSON format"}), 400
+
+# Define a route that only accepts GET requests
+@app.route('/get_endpoint', methods=['GET'])
+def get_request_handler():
+    return jsonify(data_store)
+
+if __name__ == '__main__':
+    app.run(debug=True)
